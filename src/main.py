30797b40480e92a95c90137f0a956f8d5f1bff6a
Mohamed Wael <mw86596@gmail.com>
2026-06-28 17:38:06 +0300

    add mongo client

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from routes import base, data
 from helpers import get_settings
+from motor.motor_asyncio import AsyncIOMotorClient
 
 app = FastAPI()
 settings = get_settings()
@@ -10,9 +11,16 @@ settings = get_settings()
 @asynccontextmanager
 async def lifespan():
     
-    print("Startup")
+    # Here we make connection one Mongodb
+    app.mongo_connection = AsyncIOMotorClient(settings.MONGODB_URL)
+    
+    # Here we connect on mini-rag database (where we save the data)
+    app.mongo_db = app.mongo_connection[settings.MONGODB_DATABASE_NAME]
+    
     yield
-    print("shutdown")
+    
+    # Here we close the connection
+    app.mongo_connection.close()
     
     
 app.include_router(base.base_router)
