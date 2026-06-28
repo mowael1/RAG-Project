20eced59440f8efadf4e59607ca09d5bf07f7c61
Mohamed Wael <mw86596@gmail.com>
2026-06-28 17:38:37 +0300

    make project scheme

---
+++ src/models/db_schemes/project_scheme.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, Field , field_validator, ConfigDict
+from typing import Optional
+from bson.objectid import ObjectId
+
+class project_scheme(BaseModel):
+    
+    # This to make python deal with ObjectId
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+    
+    id: Optional[ObjectId] = Field(None,alias="_id")
+    project_id: str =  Field(...,min_length=1)
+    
+    # Make validation on project_id to make sure that it's only characters or numbers
+    @field_validator
+    def validate_project_id(cls, value):
+        if not value.isalnum():
+            raise ValueError("project_id must be alphanumeric")
+        return value
+    
