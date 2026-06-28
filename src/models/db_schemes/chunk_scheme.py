50f6507b4dd91b01fca93f9aae0c1d5025f2f60f
Mohamed Wael <mw86596@gmail.com>
2026-06-28 17:38:27 +0300

    make chunk scheme

---
+++ src/models/db_schemes/chunk_scheme.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, Field , field_validator, ConfigDict
+from typing import Optional
+from bson.objectid import ObjectId
+
+class chunk_scheme(BaseModel):
+    
+    # This to make python deal with ObjectId
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+    
+    id: Optional[ObjectId] = Field(None,alias="_id")
+    chunk_text: str = Field(..., min_length=1)
+    chunk_metadata: object
+    chunk_order: int = Field(..., gt=0)
+    
+    # This will tell me this chunk belong to any project
+    chunk_project_id: ObjectId
+    # chunk_asset_id: ObjectId
+    
