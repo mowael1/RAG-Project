9625792c5ed9287454fa96c48a584a5bd39f2c3b
Mohamed Wael <mw86596@gmail.com>
2026-06-29 21:14:18 +0300

    add base data model

---
+++ src/models/BaseDataModel.py
@@ -0,0 +1,8 @@
+from helpers import get_settings
+
+class BaseDataModel:
+    
+    def __init__(self, db_client: object):
+        
+        self.db_client = db_client
+        self.app_settings = get_settings()
