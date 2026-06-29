dd0b501dd8d9a25df92c09ad0bfeaad14bca18f4
Mohamed Wael <mw86596@gmail.com>
2026-06-29 21:13:53 +0300

    solve an error

--- src/main.py
+++ src/main.py
@@ -4,24 +4,24 @@ from routes import base, data
 from helpers import get_settings
 from motor.motor_asyncio import AsyncIOMotorClient
 
-app = FastAPI()
 settings = get_settings()
 
-
 @asynccontextmanager
-async def lifespan():
+async def lifespan(app: FastAPI):
     
-    # Here we make connection one Mongodb
+    # Here we make connection on Mongodb
     app.mongo_connection = AsyncIOMotorClient(settings.MONGODB_URL)
     
     # Here we connect on mini-rag database (where we save the data)
-    app.mongo_db = app.mongo_connection[settings.MONGODB_DATABASE_NAME]
+    app.mongo_db_client = app.mongo_connection[settings.MONGODB_DATABASE_NAME]
     
     yield
     
     # Here we close the connection
     app.mongo_connection.close()
-    
-    
+
+
+app = FastAPI(lifespan=lifespan)
+
 app.include_router(base.base_router)
 app.include_router(data.data_router)
