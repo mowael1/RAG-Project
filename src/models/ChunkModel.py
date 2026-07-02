afda2df8c5346487188fd489cb2359b6734e3116
Mohamed Wael <mw86596@gmail.com>
2026-07-02 23:40:52 +0300

    add insert many function

--- src/models/ChunkModel.py
+++ src/models/ChunkModel.py
@@ -1,6 +1,8 @@
 from .BaseDataModel import BaseDataModel
 from .enums import DataBaseEnum
 from .db_schemes import chunk_scheme
+from bson.objectid import ObjectId
+from pymongo import InsertOne
 
 class ChunkModel(BaseDataModel):
     
@@ -20,4 +22,29 @@ class ChunkModel(BaseDataModel):
         
         return chunk
     
-    async def get_chunk(self, chunk_id):
+    async def get_chunk(self, chunk_id: str):
+        
+        # Return Dictionary
+        result = await self.collection.find_one({
+            "id": ObjectId(chunk_id)
+        })
+        
+        if result is None:
+            return None
+        
+        return chunk_scheme(**result)
+    
+    
+    async def insert_many_chunks(self, chunks: list,batch_size: int = 100):
+        
+        for i in range(0, len(chunks), batch_size):
+            batch = chunks[i: i+batch_size]
+            
+            operations = [
+                InsertOne(chunk.model_dump(by_alias=True, exclude_unset=True))
+                for chunk in batch
+            ]
+            
+            result = await self.collection.bulk_write(operations)
+            
+        return len(chunks)
