3b1fb3132e276779ad1bb39731c2f638296e8e8e
Mohamed Wael <mw86596@gmail.com>
2026-07-07 21:20:15 +0300

    set generation and embedding model

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 from routes import base, data
 from helpers import get_settings
 from motor.motor_asyncio import AsyncIOMotorClient
+from stores.llm import LLMProviderFactory
 
 settings = get_settings()
 
@@ -14,6 +15,18 @@ async def lifespan(app: FastAPI):
     
     # Here we connect on mini-rag database (where we save the data)
     app.mongo_db_client = app.mongo_connection[settings.MONGODB_DATABASE_NAME]
+    
+    # Provider Factory
+    llm_provider_factory = LLMProviderFactory(settings)
+    
+    # Set Generation Model
+    app.generation_client = llm_provider_factory.create(provider=settings.GENERATION_BACKEND)
+    app.generation_client.set_generation_model(settings.GENERATION_MODEL_ID)
+    
+    # Set Embedding Model
+    app.embedding_client = llm_provider_factory.create(provider=settings.EMBEDDING_BACKEND)
+    app.embedding_client.set_embedding_model(model_id= settings.EMBEDDING_MODEL_ID, 
+                                            embedding_size=settings.EMBEDDING_MODEL_SIZE)
     
     yield
     
