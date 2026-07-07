014e6c40a084ba88ccfcdd5c851cab2db6c4aac4
Mohamed Wael <mw86596@gmail.com>
2026-07-07 21:20:29 +0300

    update config file

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -11,12 +11,23 @@ class Settings(BaseSettings):
     MONGODB_URL: str
     MONGODB_DATABASE_NAME: str
     
+    GENERATION_BACKEND: str
+    EMBEDDING_BACKEND: str
     
     
-    
-    
+    OPENAI_API_URL: str
+    COHERE_API_KEY: str
+    GENERATION_MODEL_ID: str
+    EMBEDDING_MODEL_ID: str
+    EMBEDDING_MODEL_SIZE: int
     max_output_tokens: int
     temperature: float
+    limit_documents: int
+    
+    # ====================== Vector DB Config ======================
+    VECTOR_DB_BACKEND: str
+    VECTOR_DB_PATH: str
+    VECTOR_DB_DISTANCE_METHOD: str
     class Config:
         env_file = ".env"
         
