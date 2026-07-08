e177eeafa9e01c0f4a2b2569d282b7da28a327e7
Mohamed Wael <mw86596@gmail.com>
2026-07-08 12:07:55 +0300

    add nlp endpoints

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from routes import base, data
+from routes import base, data,nlp
 from helpers import get_settings
 from motor.motor_asyncio import AsyncIOMotorClient
 from stores.llm import LLMProviderFactory
+from stores.vectordb import VectorDBProviderFactory
 
 settings = get_settings()
 
@@ -16,7 +17,7 @@ async def lifespan(app: FastAPI):
     # Here we connect on mini-rag database (where we save the data)
     app.mongo_db_client = app.mongo_connection[settings.MONGODB_DATABASE_NAME]
     
-    # Provider Factory
+    # LLM Provider Factory
     llm_provider_factory = LLMProviderFactory(settings)
     
     # Set Generation Model
@@ -28,13 +29,23 @@ async def lifespan(app: FastAPI):
     app.embedding_client.set_embedding_model(model_id= settings.EMBEDDING_MODEL_ID, 
                                             embedding_size=settings.EMBEDDING_MODEL_SIZE)
     
+    # VectorDB Factory
+    vectordb_provider_factory = VectorDBProviderFactory(settings)
+    
+    # VectorDB client
+    app.vectordb_client = vectordb_provider_factory.create(provider=settings.VECTOR_DB_BACKEND)
+    app.vectordb_client.connect()
+    
+    
     yield
     
     # Here we close the connection
     app.mongo_connection.close()
+    app.vectordb_client.disconnect()
 
 
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
+app.include_router(nlp.nlp_router)
